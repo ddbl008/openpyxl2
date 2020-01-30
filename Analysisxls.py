3f3005d1750dd8c7a42771d01b735857b2cc9423
ddbl008 <magic.gt.15@hotmail.com>
2020-01-30 10:55:12 +0800

    完成对个个表列的正则表达式筛选。

--- Analysisxls.py
+++ Analysisxls.py
@@ -1,7 +1,9 @@
 import openpyxl
+import re
+
 
 class Analysisxls:
-    def __init__(self,**kwr):
+    def __init__(self, **kwr):
 
         self.workbookname = "实时电路可靠性分析工具V039.xlsm"
         self.sheetname = "全路由"
@@ -9,70 +11,67 @@ class Analysisxls:
         self.wb = openpyxl.load_workbook(self.workbookname)
         self.st = self.wb.get_sheet_by_name(self.sheetname)
 
-    def filter(self,**kwr):
-        cur=self.st.iter_rows()
-        currow=next(cur)
-        list_re=[]
-       #获取**kwr中的组成一个按顺序排列的字符串
-
+    def filter(self, **kwr):
+        cur = self.st.iter_rows()
+        currow = next(cur)
+        list_re = []
+        # 获取**kwr中的组成一个按顺序排列的字符串
 
         while currow != self.mainrowtocelltup2():
-            currow=next(cur)
+            currow = next(cur)
 
-        list_tname=[]
+        list_tname = []
 
         for i in currow:
             list_tname.append(i.value)
-        list_pd = self.listpd(list_tname)
+
+        list_pd = self.listpd(list_tname, **kwr)
 
         while currow:
-           if self.panjue(list_pd,list_cur):
-            list_re.append(list_cur)
 
+            list_cur = []
+            for i in currow:
+                list_cur.append(i.value)
 
+            if self.panjue(list_pd, list_cur):
 
-            currow=next(cur)
+                list_re.append(list_cur)
+            try:
+                currow = next(cur)
+            except:
+                return list_re
+                # currow = next(cur)
 
-        return list_re
+    def panjue(self, p1, s1):
 
-
-    def panjue(self,t1,s1):
-        re=0
-        for t,s in t1,s1:
-            if t==1:
+        for p in p1:
+            if p == 1:
                 continue
-            if t,pap<0:
-                return 0
+            else:
+                if re.match(p, s1[p1.index(p)]) is None:
+                    return 0
+
         return 1
 
-
-
-
-    def listpd(self,list_tname,**kwr):
-        list_p = len(list_tname)* [1]
+    def listpd(self, list_tname, **kwr):
+        list_p = len(list_tname) * [1]
         for key in kwr:
-            iter_li=iter(list_tname)
-            plist=next(iter_li)#type:str
+            iter_li = iter(list_tname)
+            plist = next(iter_li)  # type:str
             while plist:
-                if plist.find(key)>-1:
-                    list_p[list_tname.index(plist)]=kwr[key]
+                if plist.find(key) > -1:
+                    list_p[list_tname.index(plist)] = kwr[key]
                     break
-                plist=next(iter_li)
-
-
-
-
+                plist = next(iter_li)
 
         return list_p
 
-
-    def myinit(self,**kwr):
+    def myinit(self, **kwr):
         for a in kwr:
-            if a=="workbook":
-                self.workbookname=kwr[a]
-            elif a=="sheet":
-                self.sheetname=kwr[a]
-
+            if a == "workbook":
+                self.workbookname = kwr[a]
+            elif a == "sheet":
+                self.sheetname = kwr[a]
 
     def __str__(self):
         self.filter()
@@ -80,7 +79,6 @@ class Analysisxls:
 
     def mainrowtocelltup2(self):
         # 根据表格中第一行等于整体表格宽度来判断是否是表头别，利用生成器实现
-
 
         ll = self.st.max_column
 
@@ -103,13 +101,15 @@ class Analysisxls:
             return currow
 
 
+dicc = {"级别": "VC[3,4]$", "方向": "双向"}
+c = Analysisxls(**dicc)
+# tlist = c.mainrowtocelltup2()
+# list = []
+# for i in tlist:
+#     list.append(i.value)
+# print(list)
+a=c.filter(**dicc)
+for i in a:
+    print(i)
 
-dicc={"级别":"VC4","方向":"双向"}
-c=Analysisxls(**dicc)
-tlist=c.mainrowtocelltup2()
-list=[]
-for i in tlist:
-    list.append(i.value)
-print(list)
-print (c.listpd(list,**dicc))
-
+print(len(a))
